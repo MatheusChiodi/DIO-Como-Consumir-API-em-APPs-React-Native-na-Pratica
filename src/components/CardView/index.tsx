13a131b19cba2e3f164486ce0010ec6fb3ceb051
Matheus Chiodi <matheuschiodi20@gmail.com>
2024-01-05 13:56:12 -0300

    Criando o componente de CardView

---
+++ src/components/CardView/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, Button, Image } from 'react-native';
+
+import { styles } from './style';
+
+export default function CardView() {
+  return (
+    <View style={styles.imageContainer}>
+      <Text>CardView</Text>
+    </View>
+  );
+}
---
+++ src/components/CardView/style.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  imageContainer: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+    overflow: 'hidden',
+    borderRadius: 8,
+  },
+});
